557ed94001c4ea2e086e0b8c79e3a6588530c09b
SaileshPatel <saileshapatel@gmail.com>
2014-04-20 23:13:11 +0100

    completed some more exercises

--- ex33.py
+++ ex33.py
@@ -1,20 +1,26 @@
+# the variable 'i' has the value of zero
 i = 0
+# the list 'numbers' is empty
 numbers = []
 
+# this 'while-loop' only works if the variable 'i'
 while i < 6:
+	# the value of 'i' is printed here
 	print ("At the top i is %d" % i)
+	# the value is added to the empty list of 'numbers'
 	numbers.append(i)
 
+	# the value of i is now the value of 'i' plus 1
 	i = i + 1
+	# printing the list numbers
 	print ("Numbers now: ", numbers)
+	# printing 'i' after new value is added
 	print ("At the bottom i is %d" % i)
 
-
+# printing string
 print ("The numbers: ")
 
+# this is a for-loop which goes through the list 'numbers'
 for num in numbers:
+	# printing num
 	print (num)
-
-for i in range(0,6):
-	if i < 6:
-		print ("")
---
+++ ex34.py
@@ -0,0 +1,10 @@
+animal = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print ("The first animal in cardinal order: ", animal[0])
+print ("The third animal in ordinal order: ", animal[4])
+print ("The first animal in ordinal order: ", animal[1])
+print ("The third animal in cardinal order: ", animal[2])
+print ("The fifth animal in ordinal order: ", animal[3])
+print ("The second animal in cardinal order: ", animal[3])
+print ("The sixth animal in ordinal order: ", animal[5])
+print ("The fourth animal in cardinal order: ", animal[m4])
