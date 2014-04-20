40d39c56d54d8f76cfe3c21b5ae15b208b5ab131
SaileshPatel <saileshapatel@gmail.com>
2014-04-20 15:42:58 +0100

    added ex32.py

---
+++ ex32.py
@@ -0,0 +1,22 @@
+# created a 1-dimensional list with integer values
+the_count = [1, 2, 3, 4, 5]
+# created a 1-dimensional list with strings
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+# created a 1-dimensional list with integers and string
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this for-loop goes through a list
+for number in the_count:
+	# then prints the list
+	print ("This is count %d" % number)
+
+for fruit in fruits:
+	print ("A fruit of type: %s" % fruit)
+
+for i in change:
+	print ("I got %r" % i)
+
+# here is an empty
+elementList = (range(0, 6)
+
+print (elementList)
